fa2f15afcf7cb650d5369d8e50bcf01a3d0f8982
fidelin17 <mcfidelin@gmail.com>
2022-04-07 20:21:25 -0500

    eureka

--- ms-usuario/src/main/java/com/msusuario/feigClients/CarFeingClients.java
+++ ms-usuario/src/main/java/com/msusuario/feigClients/CarFeingClients.java
@@ -2,14 +2,12 @@ package com.msusuario.feigClients;
 
 import com.msusuario.modelos.Carros;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @FeignClient(name = "ms-carro")//url = "http://localhost:8082/carro")
+@RequestMapping("/carro")
 public interface CarFeingClients {
 
     @PostMapping()
