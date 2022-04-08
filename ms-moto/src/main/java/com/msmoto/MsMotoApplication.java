a652c64b17897e08c5cab04e968da9fd9d65d70e
fidelin17 <mcfidelin@gmail.com>
2022-04-07 19:44:42 -0500

    eureka

--- ms-moto/src/main/java/com/msmoto/MsMotoApplication.java
+++ ms-moto/src/main/java/com/msmoto/MsMotoApplication.java
@@ -2,10 +2,12 @@ package com.msmoto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class MsMotoApplication {
 
 	public static void main(String[] args) {
