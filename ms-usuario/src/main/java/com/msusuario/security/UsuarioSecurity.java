dba847173fc009347ee46acf222f074967347ed0
fidelin17 <mcfidelin@gmail.com>
2022-04-10 17:00:08 -0500

    keycloak

--- ms-usuario/src/main/java/com/msusuario/security/UsuarioSecurity.java
+++ ms-usuario/src/main/java/com/msusuario/security/UsuarioSecurity.java
@@ -1,5 +1,6 @@
 package com.msusuario.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UsuarioSecurity extends WebSecurityConfigurerAdapter {
     @Override
+    @Bean
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated()
                 .and()
